3a77ed4b3e39f8bd5afa100392e8cd5a8bc565d7
Marta Mihaylova <martamihaylovajs@gmail.com>
2021-04-16 12:01:00 +0300

    fix

--- config/routes.js
+++ config/routes.js
@@ -7,9 +7,6 @@ module.exports = (app) => {
     app.use('/auth', authController);
     app.use('/show', addAndRemoveController);
     app.use('/account', userController);
-    app.get('/', (req, res) => {
-        res.send('Hello world');
-    });
     app.get('*', (req, res) => {
         res.status(404).json({message: 'Error'});
     });
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config/config');
-// const routes = require('./config/routes');
+const routes = require('./config/routes');
 const app =  express();
 
 app.use(cors());
 require('./config/express')(app);
 require('./config/mongoose');
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
 require('./config/routes')(app);
-// app.use(routes);
+app.use(routes);
 
 
 app.listen(config.PORT, console.log.bind(console, `Server listening on port ${config.PORT}...`));
